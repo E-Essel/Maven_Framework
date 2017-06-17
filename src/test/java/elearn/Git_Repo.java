2c255c7143baadaec7ee5d4acb89f55465c0938b
eddie <e.k.essel79@gmail.com>
2017-06-17 12:50:32 +0100

    Second Test

--- src/test/java/elearn/Git_Repo.java
+++ src/test/java/elearn/Git_Repo.java
@@ -7,6 +7,12 @@ public class Git_Repo {
     public void  gitflow (){
         System.out.println("Git!");
     }
-    
+      public void gitexpmsegitsxplanation () {
+        System.out.println("Explanation");
+    }
+    public void create (){
+            System.out.println("Esteem");
+        }
+    }
 
 }
